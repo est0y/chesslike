0c8f1f91e761a0ad2dce67912b204477da3ed852
est0y <asteoon@gmail.com>
2024-04-22 07:49:04 +0300

    redesign Board structure

--- src/main/java/human/est0y/chesslike/domain/Board.java
+++ src/main/java/human/est0y/chesslike/domain/Board.java
@@ -1,23 +1,16 @@
 package human.est0y.chesslike.domain;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("all")
-@RequiredArgsConstructor
-public class Board<CELL, COLOR extends Enum<COLOR>, PIECE_TYPE extends Enum<PIECE_TYPE>, STATE> {
-    private final Map<CELL, Piece<COLOR, PIECE_TYPE, STATE>> pieces;
+public interface Board<CELL, COLOR extends Enum<COLOR>, PIECE_TYPE extends Enum<PIECE_TYPE>, STATE, STRUCTURE> {
 
-    public Piece<COLOR, PIECE_TYPE, STATE> getPiece(CELL cell) {
-        return pieces.get(cell);
-    }
+    Optional<Piece<COLOR, PIECE_TYPE, STATE>> getPiece(CELL cell);
 
-    public void putPiece(CELL cell, Piece<COLOR, PIECE_TYPE, STATE> piece) {
-        pieces.put(cell,piece);
-    }
+    void putPiece(CELL cell, Piece<COLOR, PIECE_TYPE, STATE> piece);
 
-    public void deletePiece(CELL cell){
-        pieces.remove(cell);
-    }
+    void deletePiece(CELL cell);
+
+    STRUCTURE getDataStructure();
+
 }
