d77fa2c0dd045e40c5a45e7b1bd8aa2d51797638
est0y <asteoon@gmail.com>
2024-04-26 09:26:03 +0300

    add @ToString

--- src/main/java/human/est0y/chesslike/domain/Piece.java
+++ src/main/java/human/est0y/chesslike/domain/Piece.java
@@ -4,13 +4,14 @@ package human.est0y.chesslike.domain;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
+import lombok.ToString;
 
 
 @SuppressWarnings("all")
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
+@ToString
 public class Piece<COLOR extends Enum<COLOR>, PIECE_TYPE extends Enum<PIECE_TYPE>, STATE> {
 
     private final COLOR color;
--- src/main/java/human/est0y/chesslike/domain/chess/Square.java
+++ src/main/java/human/est0y/chesslike/domain/chess/Square.java
@@ -3,10 +3,12 @@ package human.est0y.chesslike.domain.chess;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode
+@ToString
 public class Square {
     private final int x;
 
