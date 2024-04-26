38a9953e5b220c7c0ef1fb5d513e7cf174c836e5
est0y <asteoon@gmail.com>
2024-04-26 09:27:02 +0300

    add default chess State

---
+++ src/main/java/human/est0y/chesslike/domain/chess/State.java
@@ -0,0 +1,12 @@
+package human.est0y.chesslike.domain.chess;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class State {
+    private boolean isMoved;
+}
